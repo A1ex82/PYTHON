d6ee1b6629c701187de5d156c4972b611862c8dd
Alexander Navitski <navitski82@gmail.com>
2023-09-07 16:36:51 +0300

    Add task_14.py file

--- task_14.py
+++ task_14.py
@@ -1,7 +1,7 @@
 N = int(input("Введите число (N): "))
 for i in range(N + 1):
-    total = 2 ** i
+    power_of_two = 2 ** i
     if 2 ** i <= N:
-        print(total)
+        print(power_of_two)
     else:
         break
