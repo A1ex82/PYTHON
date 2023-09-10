baef34a185f2762c17d7522372f1b413e2291fe2
Alexander Navitski <navitski82@gmail.com>
2023-09-10 18:58:28 +0300

    Update task_14.py file

--- task_14.py
+++ task_14.py
@@ -1,14 +1,7 @@
-# N = int(input("Введите число (N): "))
-# for i in range(N + 1):
-#     power_of_two = 2 ** i
-#     if 2 ** i <= N:
-#         print(power_of_two)
-#     else:
-#         break
-
-from random import randrange
-
-
-# BEGIN (write your solution here)
-print(randrange(1, 11))
-# END
+N = int(input("Введите число (N): "))
+for i in range(N + 1):
+    power_of_two = 2 ** i
+    if 2 ** i <= N:
+        print(power_of_two)
+    else:
+        break
